83e3679bf7544a5db19191aa2f9938384f46f248
Xiaowen Zhang <seanxwzhang@gmail.com>
2024-02-18 19:26:10 -0800

    CORS

--- next.config.js
+++ next.config.js
@@ -13,7 +13,33 @@ module.exports = {
           {
             key: 'Permissions-Policy',
             value: 'interest-cohort=()'
-          }
+          },
+          { 
+            key: "Access-Control-Allow-Credentials",
+            value: "true" 
+          },
+          { 
+            key: "Access-Control-Allow-Origin",
+            value: "*"
+          }, // replace this your actual origin
+          { 
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS"
+          },
+          { 
+            key: "Access-Control-Allow-Headers", 
+            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" 
+          },
+        ]
+      },
+      {
+        // matching all API routes
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
       }
     ]
