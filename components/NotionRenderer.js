3c6c93bfce0078059c938dd936fd022cf707a770
Xiaowen Zhang <seanxwzhang@gmail.com>
2024-05-17 20:15:18 -0700

    fix build

--- components/NotionRenderer.js
+++ components/NotionRenderer.js
@@ -5,72 +5,17 @@ import { getTextContent } from 'notion-utils'
 import { FONTS_SANS, FONTS_SERIF } from '@/consts'
 import { useConfig } from '@/lib/config'
 import Toggle from '@/components/notion-blocks/Toggle'
+import { Code as CodeComponent } from 'react-notion-x/build/third-party/code'
 
 // Lazy-load some heavy components & override the renderers of some block types
 const components = {
   /* Lazy-load */
 
-  // Code block
-  Code: dynamic(async () => {
-    return function CodeSwitch (props) {
-      switch (getTextContent(props.block.properties.language)) {
-        case 'Mermaid':
-          return h(
-            dynamic(() => {
-              return import('@/components/notion-blocks/Mermaid').then(module => module.default)
-            }, { ssr: false }),
-            props
-          )
-        default:
-          return h(
-            dynamic(() => {
-              return import('react-notion-x/build/third-party/code').then(async module => {
-                // Additional prismjs syntax
-                await Promise.all([
-                  import('prismjs/components/prism-markup-templating'),
-                  import('prismjs/components/prism-markup'),
-                  import('prismjs/components/prism-bash'),
-                  import('prismjs/components/prism-c'),
-                  import('prismjs/components/prism-cpp'),
-                  import('prismjs/components/prism-csharp'),
-                  import('prismjs/components/prism-docker'),
-                  import('prismjs/components/prism-java'),
-                  import('prismjs/components/prism-js-templates'),
-                  import('prismjs/components/prism-coffeescript'),
-                  import('prismjs/components/prism-diff'),
-                  import('prismjs/components/prism-git'),
-                  import('prismjs/components/prism-go'),
-                  import('prismjs/components/prism-graphql'),
-                  import('prismjs/components/prism-handlebars'),
-                  import('prismjs/components/prism-less'),
-                  import('prismjs/components/prism-makefile'),
-                  import('prismjs/components/prism-markdown'),
-                  import('prismjs/components/prism-objectivec'),
-                  import('prismjs/components/prism-ocaml'),
-                  import('prismjs/components/prism-python'),
-                  import('prismjs/components/prism-reason'),
-                  import('prismjs/components/prism-rust'),
-                  import('prismjs/components/prism-sass'),
-                  import('prismjs/components/prism-scss'),
-                  import('prismjs/components/prism-solidity'),
-                  import('prismjs/components/prism-sql'),
-                  import('prismjs/components/prism-stylus'),
-                  import('prismjs/components/prism-swift'),
-                  import('prismjs/components/prism-wasm'),
-                  import('prismjs/components/prism-yaml')
-                ])
-                return module.Code
-              })
-            }),
-            props
-          )
-      }
-    }
-  }),
   // Database block
   Collection: dynamic(() => {
     return import('react-notion-x/build/third-party/collection').then(module => module.Collection)
   }),
+  Code: CodeComponent,
   // Equation block & inline variant
   Equation: dynamic(() => {
     return import('react-notion-x/build/third-party/equation').then(module => module.Equation)
@@ -88,12 +33,11 @@ const components = {
       }
     })
   }),
-
   /* Overrides */
 
-  // toggle_nobelium: ({ block, children }) => (
-  //   <Toggle block={block}>{children}</Toggle>
-  // )
+  toggle_nobelium: ({ block, children }) => (
+    <Toggle block={block}>{children}</Toggle>
+  )
 }
 
 const mapPageUrl = id => `https://www.notion.so/${id.replace(/-/g, '')}`
